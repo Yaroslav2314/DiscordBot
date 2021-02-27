47227273f4caec178e2c2f631cbe16e0585996ad
Yaroslav2314 <79742269+Yaroslav2314@users.noreply.github.com>
2021-02-27 10:17:16 +0200

    Add files via upload

---
+++ Discord-bot1.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def spam(ctx):
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone' )
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+    await ctx.send('@everyone@everyone@everyone@everyone@everyone@everyone')
+bot.run('ODE1MTE5NTk2Mjc2NDE2NTUy.YDnxRw.nnJoMyfic7h4GES4qVtNR7IMLko')
